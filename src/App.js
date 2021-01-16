085dfacddbf7da44ff2c712ef44a02a5b2c4e3ce
Front <shokooh.jk@gmail.com>
2021-01-16 17:44:40 +0330

    'folders-added'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { Router, Switch, Route, Redirect } from "react-router-dom";
+import Register from './components/register/Register';
+import SendCode from './components/sendCode/SendCode';
+import history from './components/history/history';
+import Home from './components/home/Home';
+import { LanguageProvider } from './components/container/language';
+import Authenticated from './components/authenticated/Authenticated';
+import Cookies from 'universal-cookie';
 
-function App() {
+export default function App() {
+  const [model, setModel] = useState({});
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const cookie = new Cookies()
+  const getModel = (data) => setModel(data);
+
+  {
+    const token = cookie.get('token')
+      useEffect(() => {
+        if (token !== '')
+        setIsAuthenticated(true)
+    }, [token])
+  }
+
+  const login = () => {
+    setIsAuthenticated(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <LanguageProvider>
+        <Router history={history}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/Register"  >
+              <Register getModel={getModel} />
+            </Route>
+            <Route path="/SendCode" >
+              <SendCode model={model} login={login} />
+            </Route>
+            {isAuthenticated ?
+              <Route path="/Auth" component={Authenticated} getModel={getModel} />
+              : <Redirect to='/Register' />
+            }
+          </Switch>
+        </Router>
+      </LanguageProvider>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/errorModal/ErrorModal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './errorModal.css';
+import Cookies from 'universal-cookie';
+
+export default function ErrorModal({ errorTxt, handleClose }) {
+    const cookie = new Cookies();
+    const getDirection = () => { return cookie.get('dir') }
+
+    return (
+        <div className="error-container">
+            <div style={{ direction: getDirection() }} className="error-wrapper">
+                <button onClick={handleClose} className="error-modal-btn"> X </button>
+                <p>{errorTxt}</p>
+            </div>
+        </div>
+    )
+}
